071f6b8d532c1b5f952becd6c1aef1287007922a
GHDFTG <110707241+GHDFTG@users.noreply.github.com>
2022-08-06 08:46:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,590 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME PAGE</title>
+    <link rel="stylesheet" href="./home.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&family=Koulen&family=Oleo+Script+Swash+Caps&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+<div class="nav">
+
+    <img class="logo" src="https://thumbs.dreamstime.com/b/property-construction-logo-design-home-156325391.jpg" alt="logo">
+    <h2>
+       C.A.R CONSTRUCTION COMPANY
+    </h2>
+
+    <ul class="dashboard">
+
+        <li >
+           <a class="elements" href="#section1">HOME</a> 
+        </li>
+        <li >
+          <a class="elements" href="#section4">ABOUT US</a>  
+        </li>
+        <li >
+          <a class="elements" href="#section3">PROJECTS</a>  
+        </li>
+        <li>
+           <a class="elements" href="#section5">CONTACT US</a> 
+        </li>
+      <li>
+           <a class="elements" href="#section6">REGISTER</a> 
+        </li>
+    </ul>
+    
+
+</div><hr>
+
+    <div>
+
+        <img src="https://img.freepik.com/free-vector/construction-background-construction-info-graphics-book-cover-design_40382-67.jpg?w=2000" alt="image">
+        <p class="p1">
+              LEADING AND POPULAR CONSTRUCTION COMPANY
+        </p>
+    </div>
+    <div id="section1">
+<div class="container">
+    <div class="content">
+        
+            <a
+        <h3>
+     <pre>
+                                            C AJMAL ROSHAN CONSTRUCTION COMPANY
+     </pre>
+        </h3>
+        <p>
+               C.A.R Construction was started in June 2020 with a primary aim to construct the Buildings,roads,drainages with make use of a 
+               quality products at competitive price with skilled and powerful labours.The motto of our company is to build 
+               a infrastructure that prolong for many years with modern designs,substantial base and fascinating architecture. 
+        </p>
+        
+        <p>
+              By comparing different styles and trends, our team can take different aspects of each website and 
+              combine only the best bits, so our clients receive only the best of our company.
+        </p>
+        <p>
+              Construction can be defined as the art of building something. 
+              These construction quotes will put into perspective the fact that construction can be as much an art as a requirement. 
+              
+        </p>
+        <p>
+            We’ve looked far and wide across the internet, at the good, the bad and the just plain ugly to bring you only
+            the very best of the construction industry. Read on to see our list of the very best construction industry websites of 2022.
+        </p>
+
+        <p>
+               Initially started with small group of labours,but now with a great organisation ahead with hardworking and
+               smartworking labours.
+        </p>
+       
+        <h3>
+            Support
+        </h3>
+        
+        <p>
+            We have dedicated team of co-workers who continue to work on new projects on construction of buildings,roads,drainages.
+             Our proactive attitude and delicated work has helped us to retain our customer confidence.
+             </p>
+             
+             <p>
+              This need to be continued on ordering contracts with our management work will be done on stipulated time
+                with offered cost based on architecture.
+                </p>
+              <p>
+                  You can dream, create,design, and build the most wonderful place in the world.
+                  But it requires people to make the dream a reality.
+              </P>
+             <P> 
+                 “We shape our buildings; thereafter, they shape us.”
+            </P>
+        </p>
+        
+        </a>
+    </div>
+
+    </div>
+    </div>
+</div>
+<div class="company">
+<div id="section4">
+    <p>
+          One of the Prominent company in our vicinity
+        </p>
+        <p>
+           Startup company without any background enchanced the development with the assistance of hardworking hands.
+        </p>
+        <p>
+           Necessity of young minds with foresighted thoughts to establish the company to great organisation.
+           </p> 
+           <p>
+              These construction quotes are from well-known architects and constructors and, if we look at some of 
+              these construction quotes,we will realize how much construction means to these architects.
+            </p>
+         <p>
+          When we look around us, beautiful buildings are among  our favorite things to view, coming second only after nature. 
+          Therefore, we have a lot of respect for builders, as they put in a lot of effort to complete a project to a high standard. 
+        The quotes about planning and construction worker quotes found here can inspire the people who love buildings and 
+         construction and lead them on the path of success.
+        </p>
+        <p>
+        LOCATION:PBA Arcade,
+        </p>
+        <p>
+        HMT Road,Kalamessary,
+        </p>
+        <p>
+        Ernakulam,Kerala-683104.
+        </p>
+    </p>
+</div>
+</div>
+<div id="section3">
+<img class="image2" src="https://www.chcgroup.com.sg/wp-content/uploads/2016/12/earthworks-4-1024x768.jpg" alt="image2">
+<img class="image3" src="C:\xampp\htdocs\test\image32.jpg" alt="image3">
+<p>
+ROAD CONSTRUCTION
+</p>
+<p>
+PROJECT NAME : BEENACHI PANAMARAM ROAD,WAYANAD
+</p>
+
+<p>
+This construction work is done in Beenachi Panamaram Road,Wayanad where the road and drainages properly constructed.
+</p>
+<p>
+It is great peasure that the work is completed with perfection.
+</p>
+<p>
+Construction is done with affordable cost
+</p>
+</div>
+<div>
+<img class="image5" src="file:///C:/Users/MANISHA/Downloads/WhatsApp%20Image%202022-08-05%20at%2011.19.53%20AM.jpeg" alt="image5">
+</div>
+
+<div id="section5">
+
+
+
+<div class="footer">
+    
+    <div class="left">
+        <p>
+         Construction of buildings conserned with amazing Architecture
+        </p>
+        <center>
+        <a  class="a" href="#contact us">Contact Us</a>
+    </center>
+    </div>
+
+    <div class="right">
+
+        <p>
+            Phone: +91 9360750545
+            </p>
+            <p>       
+            Mobile: +91 94422 33221
+            </p>
+           
+            <p>
+                Email Id: carconstruction@gmail.com
+            </p>
+    </div>
+    </div>
+
+</div>
+<div id="section6">
+<div class="centre">
+<p>
+   REGISTER
+</p>
+
+</body>
+<style>
+.nav
+{
+    background-color: rgba(0, 0, 0, 0.352);
+    color:white;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    display: flex;
+    align-items:right;
+
+}
+
+.dashboard
+{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+h2{
+    float:left;
+    display: block;
+    margin-right: auto;
+    margin-left: 10px;
+    font-size: 30px;
+    font-family:'Oswald', sans-serif;
+    font-weight: 900;
+}
+li{
+    list-style-type: none;
+}
+li a{
+    text-decoration: none;
+    color:blue;
+}
+
+.elements
+{
+    display: block;
+    padding: 10px;
+}
+.elements:hover
+{
+    background-color: rgb(226, 226, 141);
+    color: black;
+    cursor: pointer;
+    margin-top: -15px;
+    box-shadow: 2px 5px rgb(128, 127, 127);
+}
+
+
+.logo{
+    width: 100px;
+    height: 100px;
+    margin-left: 50px;
+}
+
+.image1
+{
+    width: 100%;
+    height: 800px;
+    position: relative;
+}
+.image2
+{
+    width: 40%;
+    height:400px;
+    margin-left:40px;
+}
+.image3
+{
+    width: 40%;
+    height: 400px;
+    margin-right:40px;
+}
+.image5
+{
+    width: 40%;
+    height: 400px;
+    padding-left:100px;
+    background-color:white;
+}
+.p1
+{
+    position: absolute;
+    width: 40%;
+    right: 30px;
+    top: 720px;
+    border: 2px solid white;
+    font-size: 35px;
+    background-color:rgb(202, 210, 167);
+    padding: 10px 30px;
+    font-weight: 500;
+    font-family: 'Roboto Mono', monospace;;
+}
+
+.container
+{
+    display: flex;
+    flex-direction: row;
+    vertical-align: top;
+    margin-bottom: 20px;
+}
+.content
+{
+    width: 60%;
+    margin-left: 50px;
+    margin-top: 100px;
+    font-size: 15px;
+    font-family: Verdana,sans-serif;
+    padding-right: 50px;
+    border-right: 2px solid rgb(206, 205, 205);
+    display: block;
+}
+
+#section1{
+    height:800px;
+    background: white;
+    padding-left:100px;
+    display:inline-block;
+    width:100%;
+}
+#section4{
+    height:500px;
+    padding-left:50px;
+    padding-right:50px;
+    background-color: grey;
+    font-family:Verdana;
+    padding-top:50px;
+    display:inline-block;
+    width:100%;
+}
+#section3{
+    height:700px;
+    padding-left:50px;
+    background-color: rgb(255,204,229);
+    padding-top:50px;
+    display:inline-block;
+    width:100%;
+}
+#section5{
+    height:700px;
+    background:rgb(192, 232, 255);
+    display:inline-block;
+    width:100%;
+}
+#section6{
+    height:400px;
+    padding-left:100px;
+    font-weight:bold;
+    background-color: grey;
+    padding-top:50px;
+    display:inline-block;
+    width:100%;
+}
+h1{
+    padding-top:50px;
+    margin:100px;
+    text-align:centre;
+    padding:0;
+}
+
+.products
+{
+    display:block;
+    margin-top: 70px;
+    width: 20%;
+    margin-left: 90px;
+    font-family: Verdana,sans-serif;
+    font-size: 15px;
+}
+
+
+table {
+    border: 2px solid black;
+    border-collapse: collapse;
+    width: 300px;
+    box-shadow: 30px 30px  rgb(199, 255, 244);
+  }
+  
+th,td {
+    padding: 8px;
+    text-align: center;
+    border-bottom: 1px solid #DDD;
+  }
+  
+  tr:hover {background-color: #D6EEEE;}
+  .footer
+  {
+      display: flex;
+      flex-direction: row;
+      height: 200px;
+      background-color: rgba(52, 126, 158, 0.363);
+      margin-top: 0px;
+      
+  }
+
+.left
+{
+    width: 55%;
+    border-right: 2px solid rgb(116, 143, 147);
+    margin-left: 50px;
+    display: block;
+    padding-right: 50px;
+}
+
+.left p{
+    font-size: 33px;
+    text-align: center;
+}
+.left .a{
+    display: block;
+    width: 100px;
+    height: 20px;
+    background-color: rgb(239, 137, 74);
+    text-decoration: none;
+    padding: 10px;
+    font-weight: 700;
+    color: black;
+    margin-top: 35px;
+    text-align: center;
+}
+
+.left .a:hover{
+    margin-top: -5px;
+}
+
+.right
+{
+    margin-left: 150px;
+    margin-top: 20px;
+    
+}
+
+.drop-down
+{
+    display: inline-block;
+    position: relative;
+
+}
+.drop-down-contents
+{
+    display: none;
+    position: absolute;
+    width: 100%;
+    overflow: auto;
+}
+.drop-down-contents a{
+    display: block;
+    color: white;
+
+}
+.drop-down:hover, .drop-down-contents{
+
+    display: block;
+}
+</style>
+</head>
+</html>
+
+
+
+
+
+
+
+
+<!DOCTYPE HTML>
+
+<html>  
+
+<body>
+
+<form action="index.php" method="post">
+
+<h4>
+
+<p>Name: <input type="varchar" name="fname"></p>
+
+<p>Email: <input type="varchar" name="femail"></p>
+
+<p>Password: <input type="varchar" name="pwd1"></p>
+
+
+<p>Phone number: <input type="varchar" name="ph"></p>
+
+
+</h4>
+
+<input type="submit">
+
+</form>
+
+</body>
+
+</html>
+
+
+index.php
+
+
+<html>
+
+<body>
+
+
+
+<?php
+error_reporting(0);
+
+$host= "localhost";
+
+$username= "root";
+
+$password = "";
+
+
+$db_name = "car2";
+
+
+$conn = mysqli_connect($host, $username, $password, $db_name);
+
+
+if (!$conn) 
+
+{
+
+	echo "Connection failed!". "<br>";
+
+}
+
+else
+
+{
+
+	echo "Connection Successful". "<br>";
+
+}
+
+$n=$_POST["fname"];
+
+$e=$_POST["femail"];
+
+$p1=$_POST["pwd1"];
+
+
+$p3=$_POST["ph"];
+
+
+$sql = "INSERT INTO tab11 (name,emailid,pass,phone)
+
+VALUES ('$n','$e','$p1','$p3')";
+
+
+if (mysqli_query($conn, $sql)) 
+{
+
+		echo "New record created successfully !";
+
+	 } else {
+
+		echo "Error: " . $sql . " " . mysqli_error($conn);
+
+}
+
+
+
+
+
+
+
+$conn->close();
+
+
+?>
+
+
+</body>
+
+</html>
+
